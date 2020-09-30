7ecff3808d65d531bc8d719053133a6ecbaf5443
Shubham <shubhamnagpal30@gmail.com>
2020-09-30 16:42:35 +0530

    Updates

---
+++ precache-manifest.eb5caa8f3e6fbeda7debed00492ae871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeda604ef151b37472d2320b85a87fd3",
+    "url": "/CD/index.html"
+  },
+  {
+    "revision": "5a29541c29188254ca09",
+    "url": "/CD/static/css/main.8ed9ebdb.chunk.css"
+  },
+  {
+    "revision": "7533af97b12fca7aac89",
+    "url": "/CD/static/js/2.b6bb2ffb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CD/static/js/2.b6bb2ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a29541c29188254ca09",
+    "url": "/CD/static/js/main.f7887554.chunk.js"
+  },
+  {
+    "revision": "f391fe75c36eacbee08c",
+    "url": "/CD/static/js/runtime-main.2d573828.js"
+  }
+]);
